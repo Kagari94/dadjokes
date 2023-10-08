e7c00f8d31d63d7d97212c4801ecf5edf74258e4
Kagari94 <jesse.hirvo@hotmail.com>
2023-10-08 16:30:35 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
+
+// Lisäsin simppelin tallennus function jolla voi pitää vitsit näkyvillä.
+
+const URL = "https://icanhazdadjoke.com/"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [joke, setJoke] = useState("Jokes here")
+  var lista = []
+
+
+
+  const fetchJoke = (e) => {
+    e.preventDefault()
+    axios.get(URL, {
+      headers: { "Accept": "text/plain" }
+    }).then((response) => {
+      setJoke(response.data)
+
+    }).catch(error => {
+      alert(error)
+    })
+
+  }
+
+  function saved(){
+    lista.push(joke)
+    for(var i = 0; i < lista.length; i++){
+      document.getElementById("lista").innerHTML += lista[i] + "<br><br>"
+    }
+  }
+
+
+return (
+  <div id='container'>
+    <form onSubmit={fetchJoke}>
+      <div>
+        <output><h4>{joke}</h4></output>
+      </div>
+      <div>
+        <button>Get a joke</button>
+        <button onClick={saved}>Save?</button>
+      </div>
+      <div>
+        <h5 id="lista"> </h5>
+      </div>
+    </form>
+  </div>
+);
 }
 
 export default App;
